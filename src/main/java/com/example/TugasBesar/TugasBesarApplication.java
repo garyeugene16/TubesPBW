ed4cc128896af1af2fff9993bd17551240b3da46
Gary Eugene <garyeugene9999@gmail.com>
2024-11-30 22:29:56 +0700

    Menambahkan initializr dan membuat tampilan awal

---
+++ src/main/java/com/example/TugasBesar/TugasBesarApplication.java
@@ -0,0 +1,13 @@
+package com.example.TugasBesar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TugasBesarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TugasBesarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TugasBesar/TugasBesarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TugasBesar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TugasBesarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
